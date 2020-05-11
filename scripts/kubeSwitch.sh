103028066f9ed29c97d42c8d88f437a5e245c845
SamKirsch10 <SamKirsch10@users.noreply.github.com>
2020-05-11 15:30:27 -0400

    allow specifying k8s context quickly

--- scripts/kubeSwitch.sh
+++ scripts/kubeSwitch.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ ! -z "$1" ]]; then
+    kubectl config use-context $1
+    exit 0
+fi
+
 echo "Choose from the following to set the k8s cluster"
 select cluster in $(kubectl config get-contexts | tail -n +2 | awk '{print $2}' | sort -n); do
     kubectl config use-context $cluster
